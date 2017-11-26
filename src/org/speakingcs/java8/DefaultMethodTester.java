24e5ece82cd77447a797ea8ffed5106a068760c6
ulasalasreenath <ulasalasreenath@gmail.com>
2017-11-27 00:30:27 +0530

    learning java 8

---
+++ src/org/speakingcs/java8/DefaultMethodTester.java
@@ -0,0 +1,45 @@
+package org.speakingcs.java8;
+
+public class DefaultMethodTester {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Car();
+        vehicle.print();
+    }
+}
+
+interface Vehicle
+{
+
+    default void print()
+    {
+        System.out.println("I am Vehicle");
+    }
+
+    static void blowHorn()
+    {
+        System.out.println("Blowing horn!!!");
+    }
+
+}
+
+interface FourWheeler
+{
+    default void print()
+    {
+        System.out.println("I am a four wheeler !");
+    }
+}
+
+class Car implements Vehicle, FourWheeler
+{
+    public void print()
+    {
+        Vehicle.super.print();
+        FourWheeler.super.print();
+        Vehicle.blowHorn();
+        System.out.println("I am a car!");
+
+    }
+
+}
